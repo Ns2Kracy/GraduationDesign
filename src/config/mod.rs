ba021fcc801307b44f4484d9f5d9e6f6008e04ee
ns2kracy <ns2kracy@gmail.com>
2024-01-26 19:07:28 +0800

    feat: 减少依赖，创建enity crate，移动logger

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -18,18 +18,10 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn load() -> anyhow::Result<Self> {
-        // check if config file exists
-        if !std::path::Path::new(&*CONFIG_PATH).exists() {
-            tracing::warn!("Config file not found, creating one at {}", &*CONFIG_PATH);
-            let config = Self::default();
-            std::fs::write(&*CONFIG_PATH, toml::to_string_pretty(&config)?)?;
-            tracing::warn!("Config file created, please edit it before restarting the server");
-            std::process::exit(0);
-        }
-
-        // load config file
-        let config = toml::from_str(&std::fs::read_to_string(&*CONFIG_PATH)?)?;
-        Ok(config)
+    pub fn load() -> anyhow::Result<Self, config::ConfigError> {
+        config::Config::builder()
+            .add_source(config::File::with_name(&CONFIG_PATH))
+            .build()?
+            .try_deserialize()
     }
 }
