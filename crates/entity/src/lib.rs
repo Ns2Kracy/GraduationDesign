3c899c17ae585aed7e6feac694e89833b8dbf376
ns2kracy <ns2kracy@gmail.com>
2024-01-28 14:40:41 +0800

    feat: 种植企业模块实体

--- crates/entity/src/lib.rs
+++ crates/entity/src/lib.rs
@@ -1,1 +1,1 @@
-
+pub mod plantation_enterprises;
--- crates/migration/src/lib.rs
+++ crates/migration/src/lib.rs
@@ -1,11 +1,10 @@
-use sea_orm_migration::{async_trait::async_trait, MigrationTrait, MigratorTrait};
+pub use sea_orm_migration::prelude::*;
 
 mod m20240128_041649_create_plantation_enterprises;
 
-
 pub struct Migrator;
 
-#[async_trait]
+#[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![Box::new(
