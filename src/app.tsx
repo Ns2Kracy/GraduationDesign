2714d6536b22b684aea79001c7c0d51905bd0bc9
ns2kracy <ns2kracy@gmail.com>
2024-02-05 18:51:10 +0800

    feat: Refactor imports and add router to App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,21 @@
 // @refresh reload
+import { Suspense } from "solid-js";
+import { MetaProvider, Title } from "@solidjs/meta";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start";
 import "./app.css";
 
 export default function App() {
 	return (
-		<main>
-			<div>
-				<h1>农产品质量管理系统</h1>
-			</div>
-		</main>
+		<Router
+			root={(props) => (
+				<MetaProvider>
+					<Title>农产品安全质量管理</Title>
+					<Suspense>{props.children}</Suspense>
+				</MetaProvider>
+			)}
+		>
+			<FileRoutes />
+		</Router>
 	);
 }
