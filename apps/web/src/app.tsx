c11db22757c3a3ba194a99c58d1fe1ab9ec46838
ns2kracy <ns2kracy@gmail.com>
2024-02-15 10:53:27 +0800

    feat: add some meta

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,6 +1,6 @@
 // @refresh reload
 import { Suspense } from "solid-js";
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import "./app.css";
@@ -10,7 +10,9 @@ export default function App() {
 		<Router
 			root={(props) => (
 				<MetaProvider>
-					<Title>农产品安全质量管理</Title>
+					<Title>农产品安全质量管理系统</Title>
+					<Meta name="description" content="农产品安全质量管理系统" />
+					<Meta name="charset" content="UTF-8" />
 					<Suspense>{props.children}</Suspense>
 				</MetaProvider>
 			)}
