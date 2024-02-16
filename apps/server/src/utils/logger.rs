5c223fca5ab11a54c4e9a0d0860fca8d946f0ee1
ns2kracy <ns2kracy@gmail.com>
2024-02-16 10:53:24 +0800

    server: update logger

--- apps/server/src/utils/logger.rs
+++ apps/server/src/utils/logger.rs
@@ -1,23 +1,23 @@
-use tracing_subscriber::{fmt::time::ChronoUtc, prelude::*, util::SubscriberInitExt};
+use tracing_subscriber::{fmt::time::ChronoLocal, prelude::*, util::SubscriberInitExt};
 
 pub async fn init_tracing() {
     let appender = tracing_appender::rolling::daily("./logs", "gd.log");
 
-    let timer = ChronoUtc::new("%F %T".to_string());
+    // use native time format
+    let timer = ChronoLocal::new("%Y-%m-%d %H:%M:%S%.3f".to_string());
 
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::fmt::layer()
                 .with_ansi(false)
-                .with_target(true)
                 .with_timer(timer.clone())
                 .with_writer(appender),
         )
         .with(
             tracing_subscriber::fmt::layer()
                 .with_ansi(true)
-                .with_target(false)
-                .with_timer(timer),
+                .with_timer(timer)
+                .pretty(),
         )
         .init();
 }
