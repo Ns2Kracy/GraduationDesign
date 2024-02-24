5359311cc148064dabca1f522ff5ceb4c3706c38
Ns2Kracy <ns2kracy@gmail.com>
2024-02-24 16:25:46 +0800

    empty fluent2 preset

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -2,6 +2,31 @@ import { definePreset } from "@pandacss/dev";
 
 export default definePreset({
 	theme: {
-		tokens: {},
+		tokens: {
+			// Radii
+			radii: {},
+			// Size
+			sizes: {},
+			// Spacing
+			spacing: {},
+			// Shadows
+			shadows: {},
+			// Typography
+			fonts: {},
+			fontSizes: {},
+			lineHeights: {},
+			fontWeights: {},
+			letterSpacings: {},
+			// Colors
+			colors: {},
+		},
+		semanticTokens: {},
 	},
+	utilities: {
+		// Stroke Width
+		strokeWidth: {},
+		// Border Radius
+		borderRadius: {},
+	},
+	patterns: {},
 });
